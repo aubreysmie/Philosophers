261c163f1ff2cf2c944945a723ee9ffa88ba60e2
aubreysmie <ekhaled@student.42.fr>
2023-10-26 14:54:37 +0200

    [FIX] Unnecessary long int use (into long unsigned)

--- philo/sources/display.c
+++ philo/sources/display.c
@@ -16,28 +16,24 @@ void	disp_action(unsigned int philo_nb, enum e_action action,
 	struct timeval ref_time)
 {
 	struct timeval	tv;
-	long int		interval;
 	static pthread_mutex_t	print_lock = PTHREAD_MUTEX_INITIALIZER;
+	long unsigned int		interval;
 
 	gettimeofday(&tv, NULL);
 	interval = (tv.tv_usec - ref_time.tv_usec)
 		+ (tv.tv_sec - ref_time.tv_sec) * 1000000;
 	pthread_mutex_lock(&print_lock);
 	if (action == TAKEN_A_FORK)
-		printf("%li %d has taken a fork\n", interval, philo_nb);
+		printf("%lu %d has taken a fork\n", interval, philo_nb);
 	if (action == THINKING)
-		printf("%li %d is thinking\n", interval, philo_nb);
+		printf("%lu %d is thinking\n", interval, philo_nb);
 	if (action == DIED)
-		printf("%li %d died\n", interval, philo_nb);
+		printf("%lu %d died\n", interval, philo_nb);
 	if (action == EATING)
-	{
-		printf("%li %d is eating\n", interval, philo_nb);
 		usleep(data->time_to_eat);
-	}
+		printf("%lu %d is eating\n", interval, philo_nb);
 	if (action == SLEEPING)
-	{
-		printf("%li %d is sleeping\n", interval, philo_nb);
 		usleep(data->time_to_sleep);
-	}
+		printf("%lu %d is sleeping\n", interval, philo_nb);
 	pthread_mutex_unlock(&print_lock);
 }
