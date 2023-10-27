a80123bb55d18e06fff53f12ece7e6b5c0ccf7ff
ekhaled <giangrandeelia@gmail.com>
2023-10-27 14:09:20 +0200

    [FIX] Wrong use of timeval pointer

--- philo/sources/display.c
+++ philo/sources/display.c
@@ -6,23 +6,26 @@
 /*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 19:05:37 by ekhaled           #+#    #+#             */
-/*   Updated: 2023/10/26 13:00:26 by ekhaled          ###   ########.fr       */
+/*   Updated: 2023/10/27 14:08:40 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 void	disp_action(unsigned int philo_nb, enum e_action action,
-	struct timeval ref_time, struct timeval *tv)
+	struct timeval ref_time, struct timeval *needed_tv)
 {
 	static pthread_mutex_t	print_lock = PTHREAD_MUTEX_INITIALIZER;
+	struct timeval			tv;
 	long unsigned int		interval;
 
-	if (tv == NULL)
-		gettimeofday(tv, NULL);
-	interval = (tv->tv_usec - ref_time.tv_usec)
-		+ (tv->tv_sec - ref_time.tv_sec) * 1000000;
 	pthread_mutex_lock(&print_lock);
+	if (action == DIED)
+		tv = *needed_tv;
+	else
+		gettimeofday(&tv, NULL);
+	interval = (tv.tv_usec - ref_time.tv_usec)
+		+ (tv.tv_sec - ref_time.tv_sec) * 1000000;
 	if (action == TAKEN_A_FORK)
 		printf("%lu %d has taken a fork\n", interval, philo_nb);
 	if (action == THINKING)
