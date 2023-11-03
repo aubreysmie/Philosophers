0b75a528be002d55e6d4c9d9cb23c1991c5e945c
aubreysmie <ekhaled@student.42.fr>
2023-11-03 03:14:31 +0100

    [CLEAN] Obsolete parts and checks

--- philo/sources/dinner.c
+++ philo/sources/dinner.c
@@ -6,7 +6,7 @@
 /*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 04:21:49 by ekhaled           #+#    #+#             */
-/*   Updated: 2023/11/03 02:12:48 by ekhaled          ###   ########.fr       */
+/*   Updated: 2023/11/03 02:22:54 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,15 @@
 void	wait_to_take_fork(t_philo *philo)
 {
 	struct timeval			tv;
-	// unsigned int			current_time;
 	unsigned int			time_since_ate;
-	// static pthread_mutex_t	print_lock;
 
-	if (!philo->data->time_to_die || !philo->data->time_to_eat)
+	if (!philo->data->time_to_die)
 		return ;
-	// pthread_mutex_lock(&print_lock);
 	gettimeofday(&tv, NULL);
-	// current_time = timeval_to_ms(tv) - timeval_to_ms(philo->data->ref_time);
 	time_since_ate = timeval_to_ms(tv)
 		- timeval_to_ms(philo->last_time_philo_ate);
 	if (philo->number_of_times_philo_has_eaten == 0)
-	{
-		// pthread_mutex_unlock(&print_lock);
 		return ;
-	}
-	// dprintf(2, "Time %u : Philo %u will wait time_to_eat\n",
-	// 	current_time, philo->number + 1);
-	// pthread_mutex_unlock(&print_lock);
 	usleep(philo->data->time_to_eat * 1000
 		* (1 - ((float) time_since_ate) / (float) philo->data->time_to_die));
 }
@@ -53,7 +43,6 @@ bool	sim_thinking(t_philo *philo)
 		if (!take_fork(philo->right_fork))
 		{
 			drop_fork(philo->left_fork);
-			// wait_to_take_fork(philo);
 			if (!should_sim_stop(philo))
 				break ;
 			continue ;
